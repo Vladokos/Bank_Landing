2375f7f16963889c8ce6ab9a67070db2a41ded70
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-09-08 22:02:04 +0400

    added check to widow resize

--- js/main.js
+++ js/main.js
@@ -3,16 +3,27 @@ const burger = document.getElementsByClassName("content-menu")[0];
 // elements which was hidden
 const header = document.getElementsByClassName("menu")[0];
 
-if (window.screen.width <= 600) {
-  header.className += " hidden";
+if (window.screen.width <= 1082) {
+  header.className = "menu hidden";
   burger.className = "content-menu";
 }
+
+window.addEventListener("resize", () => {
+  if (window.screen.width <= 1082) {
+    header.className = "menu hidden";
+    burger.className = "content-menu";
+  } else {
+    header.className = "menu";
+    burger.className = "content-menu hidden";
+  }
+});
 
 burger.children[0].addEventListener("click", () => {
   if (header.className.match(/hidden/g)) {
     burger.className += " active";
-    return (header.className = "menu");
+    header.className = "menu";
+  } else {
+    burger.className = "content-menu";
+    header.className += " hidden";
   }
-  burger.className = "content-menu";
-  return (header.className += " hidden");
 });
